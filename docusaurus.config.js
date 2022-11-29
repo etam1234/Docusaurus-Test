52bafcf4d0d24a937ce10fb339620cf6a9e73d8c
etam4225 <etam4225@bths.edu>
2022-11-29 13:30:30 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://etam1234.github.io/Docusaurus-Test/',
+  url: 'https://etam1234.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
